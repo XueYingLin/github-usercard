59e16af53e70198138f6137d0b96506004288ce8
Nikki Lin <nikkiylin@gmail.com>
2020-02-13 13:29:40 -0800

    finished the template

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -2,7 +2,16 @@
            (replacing the palceholder with your Github name):
            https://api.github.com/users/<your name>
 */
-
+ //axios.get("https://api.github.com/users/XueYingLin")
+// .then(response => {
+//   console.log(response.data)
+//   response.data.map(item => {
+//     cards.appendChild(GitHubCard);
+//   })
+// })
+//   .catch(error => {
+//     console.log('the data was not returned', error)
+//   });
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
    data in order to use it to build your component function 
@@ -13,6 +22,26 @@
 /* Step 4: Pass the data received from Github into your function, 
            create a new component and add it to the DOM as a child of .cards
 */
+// function GitHubCard(gitUrl) {
+//   //create elements
+//   const card = document.createElement('div'),
+//         name = document.createElement('p',)
+//         // image = document.createElement('img'),
+//         link = document.createElement('a');
+
+//   //create text context
+//   card.textContent = 'card';
+//   name.textContent = 'name';
+//   // image.src = gitUrl;
+//   link.src = gitUrl;
+
+//   //build structure
+//   //card.appendChild('image');
+//   card.appendChild('name');
+//   card.appendChild('link');
+
+//   return card;
+// }
 
 /* Step 5: Now that you have your own card getting added to the DOM, either 
           follow this link in your browser https://api.github.com/users/<Your github name>/followers 
@@ -23,8 +52,8 @@
           Using that array, iterate over it, requesting data for each user, creating a new card for each
           user, and adding that card to the DOM.
 */
-
-const followersArray = [];
+// const cards = document.querySelector('.cards');
+// const followersArray = [];
 
 /* Step 3: Create a function that accepts a single object as its only argument,
           Using DOM methods and properties, create a component that will return the following DOM element:
@@ -45,6 +74,64 @@ const followersArray = [];
 </div>
 
 */
+function GitHubCard(gitUrl) {
+  //create elements
+    const card = document.createElement('div'),
+          image = document.createElement('img'),
+          cardIfo = document.createElement('div'),
+          name = document.createElement('h3'),
+          userName = document.createElement('p'),
+          location = document.createElement('p'),
+          profile = document.createElement('p'),
+          a = document.createElement('a'),
+          followers = document.createElement('p'),
+          following = document.createElement('p'),
+          bio = document.createElement('p');
+
+  //create text context
+  card.textContent = 'card';
+  image.src = gitUrl;
+  cardInfo.textContent = 'cardInfo';
+  name.textContent = 'name';
+  userName.textContent = 'useName';
+  location.textContent = 'location';
+  profile.textContent = 'profile';
+  a.textContent = 'a';
+  followers.textContent = 'followers';
+  following.textContent = 'following';
+  bio.textContent = 'bio';
+
+  //build structure
+  card.appendChild('image');
+  card.appendChild('cardInfo');
+  cardInfo.appendChild('name');
+  cardInfo.appendChild('userName');
+  cardInfo.appendChild('location');
+  cardInfo.appendChild('profile');
+
+  profile.appendChild('a');
+
+  cardInfo.appendChild('followers');
+  cardInfo.appendChild('following');
+  cardInfo.appendChild('bio');
+
+
+  return card;
+}
+
+const cards = document.querySelector('.cards');
+const followersArray = [];
+
+axios.get("https://api.github.com/users/XueYingLin")
+.then(response => {
+  console.log(response.data)
+  followersArray = response.data.map(item => {
+    cards.appendChild(GitHubCard);
+  })
+})
+  .catch(error => {
+    console.log('the data was not returned', error)
+  });
 
 /* List of LS Instructors Github username's: 
   tetondan
